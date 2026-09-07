c1957656c42a8baafa355c48e2467cf08823714a
marcelomarkes690 <marcelomarkes@uol.com.br>
2026-09-07 11:46:47 -0300

    Add files via upload

---
+++ README_ULTRA.md
@@ -0,0 +1,22 @@
+# Alfabetizando TEA ULTRA
+
+Versão ampliada com 110 categorias e 11.000 atividades (100 por categoria).
+
+Novidades:
+- 30 novas categorias / 3.000 novas atividades
+- avaliação diagnóstica inicial
+- múltiplos perfis
+- modo professor e modo aluno
+- mapa de habilidades
+- recomendações pedagógicas
+- favoritos e trilhas personalizadas
+- dificuldade adaptativa
+- atividades de vida diária e autonomia
+- comunicação social e histórias sociais
+- funções executivas
+- consciência fonêmica avançada
+- matemática avançada e raciocínio
+- relatório imprimível
+- PWA instalável/offline
+
+Atualize no GitHub substituindo index.html, style.css, script.js, manifest.webmanifest, sw.js e icon.svg.
--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE='alfabetizando-tea-pro-v1';
-const ASSETS=['./','./index.html','./style.css','./script.js','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html')))));
+const C='atea-ultra-v1',A=['./','./index.html','./style.css','./script.js','./manifest.webmanifest','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(C).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html')))));
